706de54a5fa81b9dfc00050ad445382accf2bfaa
Nathaniel <vanish172@gmail.com>
2019-02-22 10:46:10 -0800

    generator for levenstein

---
+++ levenstein_distance/generator/main.go
@@ -0,0 +1,42 @@
+package main
+
+import(
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "time"
+    "math/rand"
+)
+
+
+func main(){
+    reader := bufio.NewReader(os.Stdin)
+    text, _ := reader.ReadString('\n')
+    len1,err := strconv.Atoi(text[:len(text)-1])
+    if err != nil{
+        panic(err)
+    }
+    s1 := rand.NewSource(time.Now().UnixNano())
+    rng := rand.New(s1)
+
+    out := make([]byte,len1)
+    for i := 0; i < len1; i++ {
+        out[i] = byte(97 + rng.Intn(25))
+    }
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println(string(out))
+
+    for i := 0; i < len1; i++ {
+        out[i] = byte(97 + rng.Intn(25))
+    }
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println(string(out))
+
+}
